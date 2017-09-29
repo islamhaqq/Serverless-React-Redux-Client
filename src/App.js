2d15656a45478364d6d30a2f8628351c0aee09c7
Syed Aman <sna274@nyu.edu>
2017-09-28 20:47:34 -0500

    start some user authentication code

--- src/App.js
+++ src/App.js
@@ -8,10 +8,45 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Routes from './Routes'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    // define root component state state
+    this.state = {
+      /**
+       * Whether user has logged in and verified his credentials.
+       * @type {Boolean}
+       */
+      isAuthenticated: false,
+      isAuthenticating: true
+    }
+  }
+
+  /**
+   * When the app is first mounted, check to see if the user is authenticated.
+   * @method componentDidMount
+   * @throws {Error}
+   * @return {void}
+   */
+  async componentDidMount() {
+    try {
+      // check if user is authenticated.
+    } catch (error) {
+      alert(error)
+      throw(Error)
+    }
+
+    // let the app know the user isn't authenticating anymore.
+    this.setState({ isAuthenticating: false })
+  }
+
+  // render the root component to get the app started.
   render() {
     return (
+      // inject Material UI styles
       <MuiThemeProvider>
         <div className="App">
+          {/* Child components are rendered here based on user navigation. */}
           <Routes />
         </div>
       </MuiThemeProvider>
