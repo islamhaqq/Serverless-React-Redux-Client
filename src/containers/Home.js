e05ff2957bd798a95e621e2a53414747edac128a
Syed Aman <sna274@nyu.edu>
2017-10-02 11:37:00 -0500

    create the Home container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,31 +1,22 @@
 /**
- * The home page.
+ * The home page and default route of the app.
  */
 
 import React, { Component } from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
 
-import logo from '../assets/logos/logo.svg'
-import '../assets/styles/App.css'
+import "../assets/styles/Home.css"
 
-const style = {
-  margin: 12,
-}
-
-export default class Home extends Component {
+class Home extends Component {
   render() {
     return(
-      <div>
-        <h1>Home</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <RaisedButton label="Primary" primary={true} style={style} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Home">
+        <div className="lander">
+          <h1>Scratch</h1>
+          <p>A simple note taking app</p>
+        </div>
       </div>
     )
   }
 }
+
+export default Home
