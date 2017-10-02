3f92540d1e61295c4a77f50a6602df450c2940d8
Syed Aman <sna274@nyu.edu>
2017-10-02 14:52:56 -0500

    render not found page on wildcard paths (404s & unmatched routes)

--- src/Routes.js
+++ src/Routes.js
@@ -8,10 +8,13 @@ import { Route, Switch } from 'react-router-dom'
 
 // import components to create routes for
 import Home from './containers/Home'
+import NotFound from './containers/NotFound'
 
 // define routes for every component
 export default () =>
   <Switch>
     {/* The default page of the app renders the Home component. */}
     <Route path="/" component={Home} exact />
+    {/* Wildcard path. Renders "Not Found" on 404s & unmatched routes. */}
+    <Route component={NotFound} />
   </Switch>
